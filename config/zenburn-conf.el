45cfb430a050350421e730faeadaaaaacb334e21
Stelian Iancu <stelian.iancu@gmail.com>
2013-01-13 21:40:53 +0100

    Zenburn pack completed.

---
+++ config/zenburn-conf.el
@@ -0,0 +1,5 @@
+;; Solarized theme configuration file
+
+(live-add-pack-lib "zenburn")
+(require 'zenburn-theme)
+(load-theme 'zenburn t)
---
+++ init.el
@@ -0,0 +1,4 @@
+;; Init file for zenburn-pack
+
+;; Load the zenburn color theme
+(live-load-config-file "zenburn-conf.el")
